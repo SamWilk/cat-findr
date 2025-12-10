e88ecfa4d4b7d004c4ceda04d8349a169885df01
Sam Wilk <82825157+SamWilk@users.noreply.github.com>
2025-12-10 17:33:25 -0500

    Updating file import

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -20,7 +20,7 @@ import EmailIcon from "@mui/icons-material/Email";
 import PersonIcon from "@mui/icons-material/Person";
 import FeedIcon from "@mui/icons-material/Feed";
 import { useNavigate } from "react-router";
-import MyCats from "../../components/mycats/MyCats";
+import MyCats from "../../components/mycats/mycats.jsx";
 import ReviewApplications from "../../components/reviewapplications/reviewapplications.jsx";
 
 const Profile = () => {
